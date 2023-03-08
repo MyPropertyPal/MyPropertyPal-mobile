6d13eb6d5aeca49c9c99b579ba201c8117851338
Hayden <hchalin98@gmail.com>
2023-03-08 14:42:35 -0500

    signup and login routes set

--- App.js
+++ App.js
@@ -1,24 +1,28 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import Button from "./components/Button";
-import {NavigationContainer} from '@react-navigation/native'
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import Login from "./screens/Login";
 
-const Stack = createNativeStackNavigator()
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Login from "./screens/Login";
+import SignUp from "./screens/SignUp";
+
+const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Login" component={Login} options={{headerShown: false}}/>
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="SignUp"
+          component={SignUp}
+          options={{ headerShown: false }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
-
-    // <View style={styles.container}>
-    //   <Text style={styles.text}>Welcome to MyPropertPal</Text>
-    //   <Button label="Login" />
-    //   <StatusBar style="auto" />
-    // </View>
   );
 }
 
--- screens/SignUp.js
+++ screens/SignUp.js
@@ -1,19 +1,19 @@
 import React from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
-function Login({navigation}) {
+function SignUp({navigation}) {
   return (
     <View style={styles.container}>
       <View style={styles.form}>
-        <Text style={styles.title}>Login to MyPropertyPal</Text>
-        <Button title="SignUp" onPress={()=>{
-          navigation.navigate('SignUp')
+        <Text style={styles.title}>Sign Up to MyPropertyPal</Text>
+        <Button title="Login" onPress={()=>{
+          navigation.navigate('Login')
         }}/>
       </View>
     </View>
   );
 }
 
-export default Login;
+export default SignUp;
 
 const styles = StyleSheet.create({
   container: {
@@ -33,24 +33,7 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     borderRadius: 10,
   },
-  buttonContainer: {
-    width: 200,
-    height: 58,
-    marginHorizontal: 20,
-    alignItems: "center",
-    justifyContent: "center",
-    padding: 3,
-  },
-  button: {
-    borderRadius: 10,
-    width: "100%",
-    height: "100%",
-    alignItems: "center",
-    justifyContent: "center",
-    flexDirection: "row",
-    backgroundColor: "white",
-  },
   title: {
-    paddingBottom: 10
-  }
+    paddingBottom: 10,
+  },
 });
